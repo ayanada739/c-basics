5bdeb2d33471ba0471da7db338be4e8475e23ab2
ayanada739 <ayanada739@gmail.com>
2024-07-05 00:30:37 +0300

    Nullable Type

--- c# basics/Program.cs
+++ c# basics/Program.cs
@@ -39,7 +39,104 @@ static void Main()
         #endregion
 
 
+        #region Nullable Type
 
+        #region EX01
+
+
+        // Null As a Value Is Valid Only With Reference Type
+
+        // int X = null; // Invalid
+        // int X => X Allow Only int Numbers
+
+        // int? Y = null; // Valid
+        // int? Y => Y Allow Int Values + Null
+
+        // int? Y = null;
+        //int X = 10;
+        //int? Z = Y; //InValid
+        //int? Z = X; //Valid
+
+        //int? Number; //Nullable <int> Number;
+        //Nullable --> Allow Null + Values
+
+        //decimal Num01 = 1.1M;
+        //decimal Num02 =Num01;
+
+        //decimal? Num01 = null;
+        //decimal? Num02 = null;
+
+
+        //Console.WriteLine(X);
+        //Console.WriteLine(Y);
+
+        #endregion
+
+        #region EX02
+
+        //int x = 10;
+        //int? y = /*int?*/ x; //Valid implicit casting
+        //                     //safe casting
+
+        //int? Number01 = null;
+        //int Number02 = (int)Number01; //Explicit Casting
+        //                              //UnSafe Casting
+        //                              //InValid Operation Exception
+
+        //Console.WriteLine(Number02);
+        #endregion
+
+        #region EX03
+
+        // int? K = null;
+
+        // int l;
+        //l= (int)K; -->UnSafe
+        //if (K != null)
+        // {
+        //   l = (int)K; //Casting
+
+        //  }
+        // else
+        // {
+        //     l = 0; 
+        // }
+
+        ////////////////////////////
+
+        //if (K.HasValue)
+        //{
+        //    l = K.Value; //not Casting
+
+        //}
+        //else
+        //{
+        //  l = 0;
+
+        //}
+        ///////////////////////////
+
+        //l = K.HasValue ? K.Value: 0;
+        //l = K != null ? (int) K: 0;
+
+        #endregion
+
+        #region Reference Type
+
+        //Null Is Default Value Of Reference Type
+
+        #region EX01
+        //string Messege = "Hi";
+        //Message = null; //Valid With Warning
+        //Message = null; //! --> Null Forgiveness Operator
+
+        //string? Message02 = null; //Valid Without warning
+        #endregion
+
+
+        #endregion
+
+        #endregion
 
 
 
